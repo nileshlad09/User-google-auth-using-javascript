8c5246115af1567ead52a072d283eb3b5d13df4e
Nilesh <nileshlad871@gmail.com>
2022-07-20 22:33:32 +0530

    change

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 // 1007947313665-gq0set7mjml52tucon4e65gr8gr3edo2.apps.googleusercontent.com
-
+const emailId = document.getElementById('emailId')
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log(profile);
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    const profile = googleUser.getBasicProfile();
+    emailId.innerText=profile.getEmail();
+    console.log("hi");
+    console.log('ID: ' + profile.getId()); 
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
-
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
+    console.log('Email: ' + profile.getEmail()); 
   }
