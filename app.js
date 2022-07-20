ce3803011a3f098668222225f8a7221eeaf64815
Nilesh <nileshlad871@gmail.com>
2022-07-20 22:13:44 +0530

    new

---
+++ app.js
@@ -0,0 +1,9 @@
+// 1007947313665-gq0set7mjml52tucon4e65gr8gr3edo2.apps.googleusercontent.com
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
